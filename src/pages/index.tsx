c6960b865d38e4219c1901d6d32501153037b79f
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2022-08-07 22:40:37 -0500

    refactoring and creating components folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,14 @@ import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Image from "next/image";
+import ImagePlaceHolder from "../components/ImagePlaceHolder";
+import TechnologyCard from "../components/TechnologyCard";
 import { trpc } from "../utils/trpc";
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-  const userImage = (session?.user) ? `${session?.user?.image}` : '';
+  const userImage = session?.user ? `${session?.user?.image}` : "";
 
   return (
     <>
@@ -56,7 +52,18 @@ const Home: NextPage = () => {
         {session && (
           <>
             <div>
-              <p>Hello {session.user?.name} <Image src={userImage} width={30} height={30} layout="fixed" alt="image"></Image></p>
+              <p>
+                Hello {session.user?.name}{" "}
+                <Image
+                  src={userImage}
+                  width={30}
+                  height={30}
+                  placeholder="blur"
+                  blurDataURL={ImagePlaceHolder(237, 181, 6)}
+                  layout="responsive"
+                  alt="image"
+                ></Image>
+              </p>
               <p>ID: {session.user?.id}</p>
               <p>Email: {session.user?.email}</p>
               <p>Session Expires at: {session.expires}</p>
@@ -65,27 +72,6 @@ const Home: NextPage = () => {
         )}
       </main>
     </>
-  );
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
   );
 };
 
